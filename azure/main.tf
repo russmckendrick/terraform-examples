9fea8b30d8eaf26d09d26e8068c7c10fcf090b2f
Russ McKendrick <russ@mckendrick.io>
2015-07-19 15:38:58 +0100

    Sync Config

---
+++ azure/main.tf
@@ -0,0 +1,30 @@
+# Configure the Azure Provider
+provider "azure" {
+    settings_file = "${var.azure_settings_file}"
+}
+
+resource "azure_hosted_service" "terraform-service" {
+    name = "russ-terraform-service"
+    location = "West Europe"
+    ephemeral_contents = false
+    description = "Hosted service created by Terraform."
+    label = "russ-tf-hs-01"
+}
+
+resource "azure_instance" "basic-server" {
+    name = "russ-terraform-test"
+    hosted_service_name = "${azure_hosted_service.terraform-service.name}"
+    image = "OpenLogic 7.1"
+    size = "Basic_A1"
+    storage_service_name = "russstoreage"
+    location = "West Europe"
+    username = "azureuser"
+    password = "z69rVZfH"
+
+    endpoint {
+        name = "SSH"
+        protocol = "tcp"
+        public_port = 22
+        private_port = 22
+    }
+}
---
+++ azure/variables.tf
@@ -0,0 +1,3 @@
+variable "azure_settings_file" {
+    default = "settings.publishsettings"
+}
